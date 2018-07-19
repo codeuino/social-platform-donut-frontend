6125388e233f1fa86d3f139a6e9b6fae13e7724f
jaskirat2000 <juskirat2000@gmail.com>
2018-07-20 00:12:23 +0530

    Delete db_conx.php

--- 596841401/db_conx.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_conx = mysqli_connect("localhost", "root", "", "fossworld");
-// Evaluate the connection
-if (mysqli_connect_errno()) {
-    echo mysqli_connect_error();
-    exit();
-} 
-
-?>
