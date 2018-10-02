1ffb99781a3266ca344a37045c5e6bbbe16f078c
Araan Branco <araan666@gmail.com>
2018-10-02 03:48:45 -0300

    Normalize project and add bcrypt password

--- routes/route.js
+++ routes/route.js
@@ -1,36 +1,46 @@
-const express=require('express');
-const bodyparser=require('body-parser')
-var url=bodyparser.urlencoded({extended:false});
-const passport=require('passport');
-const user=require('../schema/user.js');
-const route=express.Router();
+const express = require('express');
+const bodyparser = require('body-parser')
+const url = bodyparser.urlencoded({extended:false});
+const passport = require('passport');
+const user = require('../schema/user.js');
+const route = express.Router();
+const Bcrypt = require('bcrypt')
 
+// Bcrypt password
+const generatePassword = function(password) {
+  return Bcrypt.hashSync(password, 10)
+}
 
 //get request
-
 route.get('/',function(req,res){
-res.render('all');
+  res.render('all');
 })
-
 
 route.get('/google',passport.authenticate('google',{scope:['profile']}));
 
 route.get('/google/redirect',passport.authenticate('google'),function(req,res){
-
-
-
-    res.redirect('/profile/:id');
-
+  res.redirect('/profile/:id');
 })
 
 //post request
-
-route.post('/userlogin',url,function(req,res){
-new user({
-"fname":req.body.fname,"lname":req.body.lname,"email":req.body.email,"dob":req.body.dob,"github":req.body.github,"username":req.body.username,"pass":req.body.pass,"follower":0,"following":0,"status":'idle',"Eid":Math.floor(Math.random()*1000000)
-}).save().then(function(use){
-res.render('user',{user:use});
-})
+route.post('/userlogin', url, function(req, res){
+  new user({
+    "fname": req.body.fname,
+    "lname": req.body.lname,
+    "email": req.body.email,
+    "dob": req.body.dob,
+    "github": req.body.github,
+    "username": req.body.username,
+    "pass": generatePassword(req.body.pass),
+    "follower": 0,
+    "following": 0,
+    "status": 'idle',
+    "Eid": Math.floor(Math.random()*1000000)
+  })
+  .save()
+  .then(function(user){
+    res.render('user', { user });
+  })
 });
 
 route.get('/logout',function(req,res){
@@ -38,4 +48,4 @@ route.get('/logout',function(req,res){
   res.redirect('/');
 })
 
-module.exports=route;
+module.exports = route;
