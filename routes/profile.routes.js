6a33bca3d145df21604f3aa99de720927af8960e
harish chennupati <y17cs027@rvrjc.ac.in>
2019-03-04 22:37:42 +0530

    update user details

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const route = express.Router();
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 var url = bodyparser.urlencoded({ extended: false });
 const user = require('../schema/user.js');
 const proj = require('../schema/project.js');
 const profileController = require('../controller/profile.controller');
-var jsonParser = bodyparser.json()
-const auth = function (req, res, next) {
-    if (req.user ==null) {
-        res.redirect('/')
-    }
-    else {
-        next();
-    }
+var jsonParser = bodyparser.json();
+const auth = function(req, res, next) {
+	if (req.user == null) {
+		res.redirect('/');
+	} else {
+		next();
+	}
 };
 
-route.get("/search", url, jsonParser, profileController.search);
+route.get('/search', url, jsonParser, profileController.search);
 
 route.post('/check', url, jsonParser, profileController.check);
 
@@ -23,16 +22,17 @@ route.get('/profile/:id', auth, profileController.profileId);
 
 route.get('/profileview/:sd', auth, url, profileController.profileViewSd);
 
-route.post('/publish',auth ,url, profileController.publish);
+route.post('/publish', auth, url, profileController.publish);
 
-route.post('/upvote', auth,url, jsonParser, profileController.upvote);
+route.post('/upvote', auth, url, jsonParser, profileController.upvote);
 
 route.post('/downvote', url, jsonParser, profileController.downvote);
 
-route.get('/ch2',auth, profileController.ch2);
+route.get('/ch2', auth, profileController.ch2);
 
-route.get('/up',auth, profileController.up);
-route.get('/dashBoard',auth,url,profileController.dashBoard);
-route.get('/setting',auth,url,profileController.setting);
-route.post('/updatename',auth,jsonParser,profileController.updatename);
+route.get('/up', auth, profileController.up);
+route.get('/dashBoard', auth, url, profileController.dashBoard);
+route.get('/setting', auth, url, profileController.setting);
+route.get('/getDetails', auth, url, profileController.getDetails);
+route.post('/updateDetails', auth, jsonParser, profileController.updateDetails);
 module.exports = route;
