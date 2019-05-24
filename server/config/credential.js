7f7ebb9512398c5d4f5f786e275bfb5f31454f18
lakshyabatman <lakshya.khera@gmail.com>
2019-05-24 23:24:12 +0530

    Shifted all files to server folder

---
+++ server/config/credential.js
@@ -0,0 +1,15 @@
+module.exports = {
+  oauth: {
+    clientID: "192695093229-kf95jtdv1jge25d86ak99e68dkrpu33o.apps.googleusercontent.com",
+    clientSecret: "rXvfxBahDlJF1ORk7y660XM8"
+  },
+  database: 'mongodb://localhost:27017/donut1',
+  github: {
+    clientID: 'c9afdb238ed98b213912',
+    clientSecret: '3031efea8badea1381da682fd5f21e88e5a15d2f'
+  },
+  facebook: {
+    clientID: '383717979035587',
+    clientSecret: '82bd80d286ef6e45606da6d53a97efbc'
+  }
+}
