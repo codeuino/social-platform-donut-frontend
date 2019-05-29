5b3e24f93c0426b843c63afb8a7926603beb4724
lakshyabatman <lakshya.khera@gmail.com>
2019-05-29 13:27:17 +0530

    Small Bugs are fixed in App.vue
    added-jWT
    
    Service Worker file added, now the whole project wil be cached and Message on offline and online will be displayed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,30 @@
 <template>
-  
     <v-app>
-    <NavigationBar  /> 
-    <v-content > 
-      <router-view></router-view>
-    </v-content>
+       <div>
+         <h3 class="bg-primary text-center text-white py-2" id="offCard">
+            You're Offline, You Can Use Donut, but any changes won't be saved 
+         </h3>
+         <h3 class="bg-success text-center text-white py-2" id="onCard">
+            You're Online, Welcome Back
+         </h3>
+        <NavigationBar  /> 
+        <v-content > 
+          <router-view></router-view>
+        </v-content>
+      </div> 
   </v-app>
   
 </template>
 
 <script>
+import offline from 'v-offline'
 import NavigationBar from './components/NavigationBar.vue'
 export default {
   name: 'App',
-  components: {
+  
+  components: {  
     NavigationBar,
-    offline,
+    offline
   },
   
   data () {
@@ -25,8 +34,40 @@ export default {
     }
   },
   mounted(){
-    console.log(window)
-  }
+  },
+  created(){
+    window.addEventListener('offline', function(e) { 
+      var offlineCard = document.getElementById('offCard')
+      offlineCard.style.display="block"
+      setTimeout(function(){
+        offlineCard.style.display="none" 
+      },2000)  
+    });
+
+    window.addEventListener('online', function(e) { 
+      var offlineCard = document.getElementById('offCard')
+      offlineCard.style.display="none"    
+      let onlineCard = document.getElementById('onCard')
+      onlineCard.style.display="block" 
+      setTimeout(function(){
+        onlineCard.style.display="none" 
+      },2000)    
+     });
+  },
+  destroyed() {
+    window.removeEventListener('offline')
+    window.removeEventListener('online')
+  },
   
 }
 </script>
+
+<style scoped>
+#offCard {
+  display: none;
+}
+#onCard {
+  display: none;
+}
+</style>
+
