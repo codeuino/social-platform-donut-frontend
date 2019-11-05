e13183e247a080b29e2ebbcf57fc9cd35dbdfe7f
tushar goel <tushar.goel.dav@gmail.com>
2019-11-05 20:02:24 +0530

    Solves issue #222"
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

---
+++ server/middleware/image_upload.js
@@ -0,0 +1,13 @@
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '/uploads/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+var upload = multer({storage: storage});
+module.exports={
+    upload
+}
