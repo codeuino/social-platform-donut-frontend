e60afdc6323dfdf3fba949eabbc6f260d4e8ca16
Devesh Verma <deveshverma619@gmail.com>
2020-02-08 10:17:26 +0530

    Removed all code

--- client/src/services/ValidationService.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // eslint-disable-next-line
-  isValidEmail: (email) => (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.trim())),
-  isSamePassword: (first, second) => first.trim() === second.trim(),
-  isAgeBetween: (age, lower, upper) => lower <= age && age <= upper,
-  isTextUnderLimit: (text, max) => text.length <= max,
-  // eslint-disable-next-line
-  isValidText: (text) => (/[1234567890!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(text.trim()))
-}
