15cee2882fab6ee4ef48a1c2b92895de9d054bfd
theprover97 <theprover97@gmail.com>
2020-03-03 20:34:10 +0530

    Created Popups of Profile page (#329)
    * Added Edit-Profile Popup
    
    * Added Logout Button and Popup
    
    * Changes finalising Logout Button and its Popup
    
    * Others
    
    * Update navigation.js
    
    * Added Followers Popup
    
    * Changes in Followers popup
    
    Co-authored-by: Devesh Verma <deveshverma619@gmail.com>

--- src/user/profile/user-info/user-info.js
+++ src/user/profile/user-info/user-info.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./user-info.scss";
 import { Button } from "react-bootstrap";
-
+import {EditProfile} from "./../popups/edit-profile";
+import {Followers} from "./../popups/followers"
 class UserInfo extends Component {
+  state = { editProfile:false, followersList:false };
   render() {
+    let cancel =()=>this.setState({
+      editProfile:false
+    });
+    let cancelf =()=>this.setState({
+      followersList:false
+    });
     return (
       <div className="user-details">
         <div className="user-image">
@@ -11,12 +19,18 @@ class UserInfo extends Component {
             <img src="" alt="" />
           </div>
           <div className="edit-option">
-            <Button variant="primary">User Edit</Button>
+            <Button variant="primary" onClick={
+              ()=>this.setState({editProfile:true})}>User Edit</Button>
+              <EditProfile show={this.state.editProfile}
+              onHide={cancel} />
           </div>
         </div>
         <div className="user-data">
           <h1>
-            Dhanus Rajendra <Button variant="primary">Follow</Button>
+            Dhanus Rajendra <Button variant="primary" onClick={
+              ()=>this.setState({followersList:true})}>Follow</Button>
+              <Followers show={this.state.followersList}
+              onHide={cancelf} />
           </h1>
           <p className="profession">Front end developer</p>
           <p className="place">Bengaluru, Karnataka</p>
