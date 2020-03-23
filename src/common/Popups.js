ef73bee42c6168f84e158eef545fd2ee2cbab178
tanuj <tanujvyas10@gmail.com>
2020-03-23 14:00:42 +0530

    added UI for forgot password

--- src/common/Popups.js
+++ src/common/Popups.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { MdVerifiedUser } from 'react-icons/md';
 import { FaUserSlash } from 'react-icons/fa';
-
+ 
 class Popups extends Component {
   constructor(props){
     super(props);
     this.state = {
       show: false,
+      currentStep: 1,
       name: "",
       email: "",
       username: "",
@@ -26,26 +27,54 @@ class Popups extends Component {
       optionValue: nextProps.optionValue
     });
   }
-
+ 
   handleClose = () => {
     this.setState({show : false });
   }
-
+ 
   handleSubmit = (e) => {
     e.preventDefault();
     // send request to server to update the data 
     // ADD CODE HERE
   }
-
+ 
   handleDeactivateAccount = () => {
     console.log("Deactivating account...");
     // send request to the server to deactivate account 
     // ADD CODE HERE
   }
-
+ 
   onChange = (e) => {
     this.setState({[e.target.name] : e.target.value });
   }
+
+
+  
+  step_next = () => {
+    let currentStep = this.state.currentStep
+    currentStep = currentStep >= 2? 2: currentStep + 1
+    this.setState({
+      currentStep: currentStep
+    })
+  }
+    
+
+  
+
+
+nextButton(){
+  let currentStep = this.state.currentStep;
+  if(currentStep <2){
+    return (
+      <button 
+        className="btn btn-primary float-right" 
+        type="button" onClick={this.step_next}>
+      Next
+      </button>        
+    )
+  }
+  return null;
+}
 
   render() {
     const { option, optionValue } = this.state;
@@ -71,7 +100,7 @@ class Popups extends Component {
         </div>
       </Form>
     );
-
+ 
     const updateEmail = (
       <Form>
         <div className="row">
@@ -116,6 +145,29 @@ class Popups extends Component {
         </div>
       </Form>
     );
+    const updatePassword = (
+      <React.Fragment>
+
+      <form onSubmit={this.handleSubmit}>
+      {/* 
+        render the form steps and pass required props in
+      */}
+        <Step1 
+          currentStep={this.state.currentStep} 
+          onChange={this.onChange}
+          email={this.state.email}
+        />
+        <Step2 
+          currentStep={this.state.currentStep} 
+          onChange={this.onChange}
+         
+        />
+      
+        {this.nextButton()}
+
+      </form>
+      </React.Fragment>
+    );
     const deactivateAccount = (
       <div className="container">
         <p className="text text-center" style={{color: "red"}}><b>Are you sure ??</b></p>
@@ -128,7 +180,7 @@ class Popups extends Component {
         </div>
       </div>
     );
-
+ 
     const checkVerification = (
       <div className="container">
         <div className="row">
@@ -150,25 +202,77 @@ class Popups extends Component {
           {Boolean(option === "name") ? updateName : null}
           {Boolean(option === "email") ? updateEmail: null}
           {Boolean(option === "username") ? updateUsername: null}
+          {Boolean(option === "password") ? updatePassword : null}
           {Boolean(option === "account") ? deactivateAccount: null}
           {Boolean(option === "identity") ? checkVerification : null}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
+        
         </Modal.Footer>
       </Modal>
     )
   }
 }
 
+
+function Step1(props) {
+  if (props.currentStep !== 1) {
+    return null
+  } 
+  return(
+    <div className="form-group">
+      <label htmlFor="email">Registered-Email address</label>
+      <input
+        className="form-control"
+        id="email"
+        name="email"
+        type="text"
+        placeholder="abc@gmail.com"
+        value={props.email}
+        onChange={props.onChange}
+        />
+    </div>
+  );
+}
+
+
+
+function Step2(props) {
+  if (props.currentStep !== 2) {
+    return null
+  } 
+  return(
+    <React.Fragment>
+    <div className="form-group">
+      <label htmlFor="password">New Password</label>
+      <input
+        className="form-control"
+        name="new_password"
+        type="password"
+        placeholder="***********"
+        onChange={props.onChange}
+        />      
+    </div>
+    <div className="form-group">
+    <label htmlFor="password">Confirm Password</label>
+    <input
+      className="form-control"
+      name="confirm_password"
+      type="password"
+      placeholder="***********"
+      onChange={props.onChange}
+      />      
+  </div>
+    <button className="btn btn-primary btn-block">Update Password</button>
+    </React.Fragment>
+  );
+}
+ 
 Popups.propTypes = {
   modalShow: PropTypes.bool.isRequired,
   option: PropTypes.string.isRequired,
   optionValue: PropTypes.any.isRequired
 }
-
-
+ 
+ 
 export default Popups;
-
