ee736cf41824bb0c9a11428ade258965b4055f10
dhanushrajendra <dhanush.r@shawacademy.com>
2020-03-25 19:15:06 +0530

    resolving all console errors

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <Router />
       </React.Fragment>
     );
   }
 }
-document.title = 'Donut';
+document.title = "Donut";
 
 export default App;
