25c76c8a63991b422599fc40387bfcd68dd56938
mrkhst <iit2016126@iiita.ac.in>
2020-04-09 22:10:04 +0530

    Upcoming events component to Material-UI
    changes
    
    build error
    
    code enhancement
    
    Enhancement
    
    code error

--- src/user/dashboard/upcoming-events/upcoming-events.js
+++ src/user/dashboard/upcoming-events/upcoming-events.js
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
 import "./upcoming-events.scss";
 import events from '../../../jsonData/upcoming-events';
+import { List, ListItem, ListItemText, ListItemAvatar, Avatar, Divider, Typography } from '@material-ui/core';
+
 
 class UpcomingEvents extends Component {
   render() {
     let upcomingEvents = events.map((event,i)=>{
       return (
-        <div key={i}>
-          <div className="event-container"> 
-            <div className="img-container">
-              <img alt="event-icon" src={event.imgSrc}></img>
-            </div>
-            <div className="event-description">
-              <h6>{event.createdBy}</h6>
-              {event.tag ? <button type="button" className="tag">{event.tag}</button> : <div></div>}
-              <p>{event.description}</p>
-            </div>
-          </div>
-        </div>
+      <>
+        <ListItem key={i} alignItems="flex-start" className="list-item">
+          <ListItemAvatar className="profile-pic">
+            <Avatar alt="Profile" src={event.imgSrc} className="icon"/>
+          </ListItemAvatar>
+          <ListItemText
+            primary={<Typography style={{"fontWeight": "600", "fontSize": "15px"}}>{event.createdBy}</Typography>}
+            secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                color="textPrimary"
+              >
+              </Typography>
+              <p style={{"fontSize": "13px"}}>{event.description}</p>
+            </React.Fragment>
+          }
+          />
+          {event.tag ? <button variant="contained" color="primary" component="span" className="tag">
+            {event.tag}
+          </button> : <div></div>}
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </>
       )
     })
     return (
@@ -26,9 +41,11 @@ class UpcomingEvents extends Component {
           <h5>Upcoming Events</h5>
         </div>
         <div className="all-events">
-          {upcomingEvents}
+          <List className="list">
+            {upcomingEvents}
+          </List>
         </div>
-      </div>
+    </div>
     );
   }
 }
