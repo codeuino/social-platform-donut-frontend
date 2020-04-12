560135cee6096f37b6c609a5a8ac7af67c807094
tanuj <tanujvyas10@gmail.com>
2020-04-12 13:29:06 +0530

    json-data file addition

--- src/user/dashboard/updates/updates.js
+++ src/user/dashboard/updates/updates.js
@@ -1,84 +1,45 @@
 import React, { Component } from "react";
 import "./updates.scss";
-import gsoc from "../../../images/gsoc.png";
-
+import organizationUpdates from '../../../jsonData/organization-updates'
+import { makeStyles,List,ListItem,ListItemText,ListItemAvatar,Avatar,Divider } from '@material-ui/core';
 class Updates extends Component {
+
+
   render() {
+
+    const useStyles = makeStyles((theme) => ({
+      root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+      },
+      inline: {
+        display: 'inline',
+      }
+    }));
+
+    let updates = organizationUpdates.map((item)=>(
+    <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+    <Avatar alt="" src={item.imgSrc} />
+  </ListItemAvatar>
+    <ListItemText
+      primary={item.title}
+      secondary={
+        <React.Fragment>
+          {item.description}
+          <Divider component="li" />
+        </React.Fragment>
+      }
+    />
+  </ListItem>
+    ))
     return (
       <div className="updates">
         <div className="org-updates">
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
-          <div className="notification">
-            <div className="image">
-              <img src={gsoc} alt="icon" />
-            </div>
-            <div className="img-desc">
-              <h2>We got into Gsoc</h2>
-              <p>No errors to display with the current filters applied.</p>
-            </div>
-          </div>
+        <List className={useStyles.root}>
+        {updates}
+        </List>
         </div>
       </div>
     );
