c40a4b3bca4259e56bb7150d67d03b96666ae954
mrkhst <iit2016126@iiita.ac.in>
2020-04-13 00:20:44 +0530

    Added portfolio JSON and changes the UI to material-UI
    build error solving

--- src/user/dashboard/portfolio/portfolio.js
+++ src/user/dashboard/portfolio/portfolio.js
@@ -1,40 +1,55 @@
 import React, { Component } from "react";
 import "./portfolio.scss";
+import { data_1, data_2, data_3 } from '../../../jsonData/portfolio-feed'
+import { Typography, ListItemText, ListItem, List } from '@material-ui/core';
+
+
 
 class Portfolio extends Component {
   render() {
+    let portfolio_1 = data_1.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography style={{"fontWeight": "bold", "fontSize": "36px"}}>{item.contentValue}</Typography>}
+          secondary={<Typography style={{"fontWeight": "600", "fontSize": "14px", "color": "#4457A5"}}>{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
+
+    let portfolio_2 = data_2.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography style={{"fontWeight": "bold", "fontSize": "36px"}}>{item.contentValue}</Typography>}
+          secondary={<Typography style={{"fontWeight": "600", "fontSize": "14px", "color": "#4457A5"}}>{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
+
+    let portfolio_3 = data_3.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography style={{"fontWeight": "bold", "fontSize": "36px"}}>{item.contentValue}</Typography>}
+          secondary={<Typography style={{"fontWeight": "600", "fontSize": "14px", "color": "#4457A5"}}>{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
     return (
       <div className="portfolio">
-        <div className="items-list">
-          <div className="item">
-            <h1><b>41</b></h1>
-            <h3>Donuts</h3>
-          </div>
-          <div className="item">
-            <h1><b>59</b></h1>
-            <h3>Events Organized</h3>
-          </div>
-        </div>
-        <div className="items-list">
-          <div className="item">
-            <h1><b>123</b></h1>
-            <h3>Followers</h3>
-          </div>
-          <div className="item">
-            <h1><b>87</b></h1>
-            <h3>Events Attended</h3>
-          </div>
-        </div>
-        <div className="items-list">
-          <div className="item">
-            <h1><b>12</b></h1>
-            <h3>Projects</h3>
-          </div>
-          <div className="item">
-            <h1><b>41</b></h1>
-            <h3>Following</h3>
-          </div>
-        </div>
+        <List className="items-list">
+          {portfolio_1}
+        </List>
+        <List className="items-list">
+          {portfolio_2}
+        </List>
+        <List className="items-list">
+          {portfolio_3}
+        </List>
       </div>
     );
   }
