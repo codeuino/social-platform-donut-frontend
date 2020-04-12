c709a4e0b262c3aa074bb140405acb2556a81a8f
mrkhst <iit2016126@iiita.ac.in>
2020-04-13 03:33:55 +0530

    Added portfolio JSON and changed the UI to material-UI
    css using withStyles

---
+++ src/jsonData/portfolio-feed.js
@@ -0,0 +1,38 @@
+const data_1 = [
+    {
+        contentType: 'Donuts',
+        contentValue: 41
+    },
+    {
+        contentType: 'Followers',
+        contentValue: 123
+    },
+]
+
+const data_2 = [
+   {
+        contentType: 'Projects',
+        contentValue: 12
+    },
+    {
+        contentType: 'Events Organized',
+        contentValue: 59
+    }
+]
+
+const data_3 = [
+    {
+        contentType: 'Events Attended',
+        contentValue: 87
+    },
+    {
+        contentType: 'Following',
+        contentValue: 41
+    }
+]
+
+module.exports = {
+    data_1: data_1,
+    data_2: data_2,
+    data_3: data_3
+}
