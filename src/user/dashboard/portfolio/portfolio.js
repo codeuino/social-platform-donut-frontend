fb409feb342e0a71fb000a69381eee592b75ee07
Rakshit <30564496+rak-shit@users.noreply.github.com>
2020-04-18 17:05:30 +0530

    Added portfolio JSON and changed the UI to material-UI (#424)
    css using withStyles

--- src/user/dashboard/portfolio/portfolio.js
+++ src/user/dashboard/portfolio/portfolio.js
@@ -1,43 +1,75 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import "./portfolio.scss";
+import { data_1, data_2, data_3 } from '../../../jsonData/portfolio-feed'
+import { Typography, ListItemText, ListItem, List, withStyles } from '@material-ui/core';
+
+
+const styles = theme => ({
+  pri: {
+    fontWeight: "bold",
+    fontSize: "36px"
+  },
+  sec: {
+    fontSize: "14px",
+    fontWeight: "600",
+    color: "#4457A5"
+  }
+});
 
 class Portfolio extends Component {
   render() {
+    const { classes } = this.props
+    let portfolio_1 = data_1.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography className={classes.pri} >{item.contentValue}</Typography>}
+          secondary={<Typography className={classes.sec} >{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
+
+    let portfolio_2 = data_2.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography className={classes.pri} >{item.contentValue}</Typography>}
+          secondary={<Typography className={classes.sec} >{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
+
+    let portfolio_3 = data_3.map((item, i) => {
+      return (
+        <ListItem className="item" key={i}>
+          <ListItemText className="list-text"
+          primary={<Typography className={classes.pri} >{item.contentValue}</Typography>}
+          secondary={<Typography className={classes.sec} >{item.contentType}</Typography>}
+           />
+        </ListItem>
+      )
+    })
     return (
       <div className="portfolio">
-        <div className="items-list">
-          <div className="item">
-            <h1><b>41</b></h1>
-            <h3>Donuts</h3>
-          </div>
-          <div className="item">
-            <h1><b>59</b></h1>
-            <h3>Events Organized</h3>
-          </div>
-        </div>
-        <div className="items-list">
-          <div className="item">
-            <h1><b>123</b></h1>
-            <h3>Followers</h3>
-          </div>
-          <div className="item">
-            <h1><b>87</b></h1>
-            <h3>Events Attended</h3>
-          </div>
-        </div>
-        <div className="items-list">
-          <div className="item">
-            <h1><b>12</b></h1>
-            <h3>Projects</h3>
-          </div>
-          <div className="item">
-            <h1><b>41</b></h1>
-            <h3>Following</h3>
-          </div>
-        </div>
+        <List className="items-list">
+          {portfolio_1}
+        </List>
+        <List className="items-list">
+          {portfolio_2}
+        </List>
+        <List className="items-list">
+          {portfolio_3}
+        </List>
       </div>
     );
   }
 }
 
-export default Portfolio;
+Portfolio.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Portfolio);
