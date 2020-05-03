235766460db586b08cd36905c046302d66344461
tanujvyas10 <48439116+tanujvyas10@users.noreply.github.com>
2020-05-03 11:21:44 +0530

    Added UI for the entire event section [WIP] (#463)
    * package-json filtered
    
    * redesign the event-section
    
    * formatting

--- src/user/projects/proj-info/proj-info.js
+++ src/user/projects/proj-info/proj-info.js
@@ -40,11 +40,6 @@ class ProjInfo extends Component {
       root: {
         flexGrow: 1,
       },
-      paper: {
-        padding: theme.spacing(2),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-      },
     }));
 
     const useStyles2 = makeStyles({
