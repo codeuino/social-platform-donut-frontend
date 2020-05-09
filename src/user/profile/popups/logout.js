294174596aeaa2b9c2f37232cbfa4864e01f3eaf
Rupeshiya <rupeshiya@gmail.com>
2020-05-09 13:16:22 +0530

    fixed some client side issues and integrated backend

--- src/user/profile/popups/logout.js
+++ src/user/profile/popups/logout.js
@@ -24,10 +24,10 @@ class Logout extends Component{
             centered>
           <div className="container">
             <Modal.Header closeButton className="heading border border-0 p-0">
-            <Modal.Title id="contained-modal-title-vcenter"> 
-            <div className="title">Logout?</div>
-            <div className="message">Are you sure you want to logout of Donut?</div>
-            </Modal.Title> 
+              <Modal.Title id="contained-modal-title-vcenter"> 
+                <div className="title">Logout?</div>
+                <div className="message">Are you sure you want to logout of Donut?</div>
+              </Modal.Title> 
             </Modal.Header>
             
             <div className="form-footer">
