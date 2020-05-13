fd99209eb03e63c6db43d27d19d9709139d07bf0
Vaibhav D. Aren <vaibhavdaren@gmail.com>
2020-05-13 12:13:05 +0530

    Revert "Refactoring Frontend components (#391)" (#472)
    This reverts commit 9d110373fced6a583d4cae179b6e7473f80ff6ad.

---
+++ src/user/dashboard/updates/updates.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "./updates.scss";
+import gsoc from "../../../images/gsoc.png";
+
+class Updates extends Component {
+  render() {
+    return (
+      <div className="updates">
+        <div className="org-updates">
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+          <div className="notification">
+            <div className="image">
+              <img src={gsoc} alt="icon" />
+            </div>
+            <div className="img-desc">
+              <h2>We got into Gsoc</h2>
+              <p>No errors to display with the current filters applied.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Updates;
