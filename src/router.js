8d799932514086c0072d7e01dbbfa5e7c38dd304
Asel <peirisasel373@gmail.com>
2020-05-15 21:49:09 +0530

    GSoC frontend - Proposal dashboard page implementation

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ import ProjInfo from "./user/projects/proj-info/proj-info";
 import PrivateRoute from "./common/PrivateRoute";
 import Maintenance from "./maintenance/maintenance";
 import Events from "./user/events/events";
-
+import UserProposalDashboard from "./user/proposals/UserProposalDashboard/UserProposalDashboard";
 
 const Router = () => (
   <BrowserRouter>
@@ -25,9 +25,10 @@ const Router = () => (
       <PrivateRoute exact path="/profile" component={Profile} />
       <PrivateRoute exact path="/:id/proj-info" component={ProjInfo} />
       <PrivateRoute exact path="/organization" component={Organization} />
-      <PrivateRoute exact path="/settings" component={Settings}/>
-      <PrivateRoute exact path="/projects" component={Projects}/>
-      <PrivateRoute exact path="/events" component={Events}/>
+      <PrivateRoute exact path="/settings" component={Settings} />
+      <PrivateRoute exact path="/projects" component={Projects} />
+      <PrivateRoute exact path="/events" component={Events} />
+      <PrivateRoute exact path="/proposal" component={UserProposalDashboard} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
