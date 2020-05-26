5c155403a363219365fd8e589f197212348af6ad
Rupesh Krishna Jha <rupeshiya@gmail.com>
2020-05-27 00:28:54 +0530

    added admin register page (#481)

--- src/router.js
+++ src/router.js
@@ -12,6 +12,7 @@ import ProjInfo from "./user/projects/proj-info/proj-info";
 import PrivateRoute from "./common/PrivateRoute";
 import Maintenance from "./maintenance/maintenance";
 import Events from "./user/events/events";
+import Admin from "./user/Admin/Admin";
 import Setup from "./user/setup/Setup";
 import CommunitySetting from "./user/dashboard/Community/CommunitySetting";
 import Insight from "./user/dashboard/insights/Insight";
@@ -28,12 +29,13 @@ const Router = () => (
       <PrivateRoute exact path="/profile" component={Profile} />
       <PrivateRoute exact path="/:id/proj-info" component={ProjInfo} />
       <PrivateRoute exact path="/organization" component={Organization} />
-      <PrivateRoute exact path="/settings" component={Settings} />
+      <PrivateRoute exact path="/settings" component={Settings}/>
       <PrivateRoute exact path="/projects" component={Projects} />
       <PrivateRoute exact path="/events" component={Events} />
       <PrivateRoute exact path="/setup" component={Setup} />
       <PrivateRoute exact path="/org-settings" component={CommunitySetting} />
-       <PrivateRoute exact path="/insight" component={Insight} />
+      <PrivateRoute exact path="/insight" component={Insight} />
+      <PrivateRoute exact path="/admin" component={Admin}/>
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
