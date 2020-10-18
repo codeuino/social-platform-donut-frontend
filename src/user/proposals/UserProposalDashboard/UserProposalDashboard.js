a87ee14001e01e9cd2265ba7c9572d15b7cf94a7
lazycipher <17141301+lazycipher@users.noreply.github.com>
2020-10-18 19:03:15 +0530

    UserProposalDashboard Responsiveness Fix

--- src/user/proposals/UserProposalDashboard/UserProposalDashboard.js
+++ src/user/proposals/UserProposalDashboard/UserProposalDashboard.js
@@ -14,17 +14,17 @@ class UserProposalDashboard extends Component {
   }
   render() {
     return (
-      <div className="proposal_dashboard">
-        <div className="proposal_dashboard__navigation">
-          <Navigation dashboard={this.state.dashboard} />
+      <>
+        <Navigation dashboard={this.state.dashboard} />
+        <div className="proposal_dashboard">
+          <div className="proposal_dashboard__content">
+            <DashboardContent />
+          </div>
+          <div className="proposal_dashboard__rightpanel">
+            <DashboardRightPanel />
+          </div>
         </div>
-        <div className="proposal_dashboard__content">
-          <DashboardContent />
-        </div>
-        <div className="proposal_dashboard__rightpanel">
-          <DashboardRightPanel />
-        </div>
-      </div>
+      </>
     );
   }
 }
